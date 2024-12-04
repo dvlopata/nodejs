0853d6099bf446a85a3217433626ffead7b4a500
dvlopata <162029648+dvlopata@users.noreply.github.com>
2024-12-04 18:27:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-#!/usr/bin/env groovy
 pipeline {
     agent any
     stages {
         stage('Install') {
             steps {
-                echo 'Install...'
+                echo 'Installing dependencies...'
                 sh 'npm install'
             }
         }
         stage('Stop all') {
             steps {
-                echo 'Stop all...'
-                sh 'forever stopall'
+                echo 'Stopping all running instances...'
+                sh 'forever stopall || true'
             }
         }
         stage('Deploy') {
             steps {
-                echo 'Deploying...'
+                echo 'Starting application...'
                 sh 'BUILD_ID=dontKillMe pm2 start app.js'
             }
         }
